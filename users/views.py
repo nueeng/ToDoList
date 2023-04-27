db5ee3b09c35b2c7976eaedc6d043627540740ee
Choi Junyoung <nueeeng@gmail.com>
2023-04-27 23:02:04 +0900

    signup api

--- users/views.py
+++ users/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework import permissions
+from rest_framework.views import APIView
+from rest_framework.generics import get_object_or_404
+from rest_framework.response import Response
+from users.models import User
+from users.serializers import UserSerializer
 
-# Create your views here.
+
+class UserView(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message":"가입완료!"}, status=status.HTTP_201_CREATED)
+        else:
+            return Response({"message":f"{serializer.errors}"}, status=status.HTTP_400_BAD_REQUEST)
+        
